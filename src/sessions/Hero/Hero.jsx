6357b2385d2b82079369f5ef0150b8ee81923282
Misfarz <misfaryousaf@gmail.com>
2024-07-24 20:24:03 +0530

    m1

--- src/sessions/Hero/Hero.jsx
+++ src/sessions/Hero/Hero.jsx
@@ -15,7 +15,7 @@ function Hero() {
 
       <h1>Mohammed misfar<br/> Yusaf c</h1>
       <h2>Frontend Developer</h2>
-      <span>
+      <span className='small-logos'>
         <a href='https://www.instagram.com/misfar_bin_yusaf/' target='_blank'><img src={instaDark}  alt="twitter icon"  className='insta-light'/></a>
         <a href='https://www.linkedin.com/in/mohammed-misfar-41a78a279/'target='_blank'><img src={linkedinIcon} alt="linkedin icon" /></a>
         <a href='https://github.com/Misfarz' target='_blank'><img src={githubIcon} alt="github icon" /></a>
